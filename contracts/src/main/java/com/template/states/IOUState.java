02ebac1b6bf6f15afa29620d2e94fe91c1b830f6
Vyacheslav Korotach <vkorotach@gmail.com>
2021-01-14 04:30:30 +0200

    status added

--- contracts/src/main/java/com/template/states/IOUState.java
+++ contracts/src/main/java/com/template/states/IOUState.java
@@ -3,8 +3,12 @@ package com.template.states;
 import com.template.contracts.TemplateContract;
 import net.corda.core.contracts.BelongsToContract;
 import net.corda.core.contracts.ContractState;
+import net.corda.core.contracts.UniqueIdentifier;
 import net.corda.core.identity.AbstractParty;
+import net.corda.core.identity.AnonymousParty;
 import net.corda.core.identity.Party;
+import net.corda.core.serialization.ConstructorForDeserialization;
+import net.corda.core.serialization.CordaSerializable;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,11 +22,34 @@ public class IOUState implements ContractState {
     private final int value;
     private final Party lender;
     private final Party borrower;
+    private Status status;
 
     public IOUState(int value, Party lender, Party borrower) {
         this.value = value;
         this.lender = lender;
         this.borrower = borrower;
+        this.status = Status.GAME_IN_PROGRESS;
+    }
+
+    @ConstructorForDeserialization
+    public IOUState(int value, Party lender, Party borrower, Status status) {
+        this.value = value;
+        this.lender = lender;
+        this.borrower = borrower;
+        this.status = status;
+    }
+
+    @CordaSerializable
+    public enum Status {
+        GAME_IN_PROGRESS, GAME_OVER
+    }
+
+    public IOUState returnNewState(Integer i){
+        if(i > 2){
+            IOUState b = new IOUState(this.value,this.lender,this.borrower,Status.GAME_OVER);
+            return b;
+        }
+        return null;
     }
 
     public int getValue() {
